c68442430d5c1233605df75acf6bc420e955c672
EC2 Default User <ec2-user@ip-172-31-27-32.us-east-2.compute.internal>
2019-09-28 17:39:38 +0000

    make it better3

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,8 +3,4 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
-  
-  def highlight_if_current_column(column)
-    params[:sort_by] == column.to_s ? 'hilite' : nil
-  end
 end
